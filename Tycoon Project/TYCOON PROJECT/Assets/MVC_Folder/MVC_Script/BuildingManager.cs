66073c5eb2efd736919fa4869ec26fc3fbd52144
MathGD <m.vancappellen@rubika-edu.com>
2022-04-11 18:54:06 +0200

    Grid System + Adding Structure

---
+++ Tycoon Project/TYCOON PROJECT/Assets/MVC_Folder/MVC_Script/BuildingManager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BuildingManager : MonoBehaviour
+{
+    public GameObject[] objects;
+    private GameObject pendingObject;
+
+    private Vector3 pos;
+
+    private RaycastHit hit;
+    [SerializeField] private LayerMask layerMask;
+
+    public float rotateAmount;
+
+    public float gridSize;
+    bool gridOn = true;
+    [SerializeField] private Toggle gridToggle;
+
+    private void Update()
+    {
+        if (pendingObject != null)
+        {
+            if (gridOn)
+            {
+                pendingObject.transform.position = new Vector3(
+                    RoundToNearestGrid(pos.x),
+                    RoundToNearestGrid(pos.y),
+                    RoundToNearestGrid(pos.z)
+                    );
+            }
+            else { pendingObject.transform.position = pos; }
+
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                PlaceObject();
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                RotateObject();
+            }
+        }
+    }
+
+    public void PlaceObject()
+    {
+        pendingObject = null;
+    }
+
+    public void RotateObject()
+    {
+        pendingObject.transform.Rotate(Vector3.up, rotateAmount);
+    }
+
+    private void FixedUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask))
+        {
+            pos = hit.point;
+        }
+    }
+
+    public void SelectObject(int index)
+    {
+        pendingObject = Instantiate(objects[index], pos, transform.rotation);
+    }
+
+    public void ToggleGrid()
+    {
+        if (gridToggle.isOn)
+        {
+            gridOn = true;
+        }
+        else { gridOn = false; }
+    }
+
+    float RoundToNearestGrid(float pos)
+    {
+        float xDiff = pos % gridSize;
+        pos -= xDiff;
+        if (xDiff > (gridSize / 2))
+        {
+            pos += gridSize;
+        }
+        return pos;
+    }
+}
