7a709e2fde8816c2c0771085d17a076ad40c6cde
MathGD <m.vancappellen@rubika-edu.com>
2022-05-12 18:44:53 +0200

    Add CarController

---
+++ Tycoon Project/TYCOON PROJECT/Assets/MVC_Folder/MVC_Script/CarController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarController : MonoBehaviour
+{
+    public NavMeshAgent agentCar;
+    public bool canDrive = true;
+
+    public Transform targetedParkingPlace;
+
+    void Update()
+    {
+        if (canDrive)
+        {
+            agentCar.SetDestination(targetedParkingPlace.position);
+        }
+    }
+}
